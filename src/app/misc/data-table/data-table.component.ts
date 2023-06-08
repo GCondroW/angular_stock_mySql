4e1125a4962b40e5642e06c2e187b4d95e93ee7a
GCondroW <GIGIHCONDRO@GMAIL.COM>
2023-06-08 16:30:09 +0700

    misc
    -misc

--- src/app/misc/data-table/data-table.component.ts
+++ src/app/misc/data-table/data-table.component.ts
@@ -10,34 +10,17 @@ import { CellClickedEvent, ColDef, GridReadyEvent, GridOptions } from 'ag-grid-c
 export class DataTableComponent implements OnInit {
 	ngOnInit(){
 		console.log(this);
-
-		this.gridOptions= {
-			columnDefs: this.tableData.getColumnDefs(),
-			pagination: true,
-			rowSelection: 'single',
-			defaultColDef: this.defaultColDef,
-			onGridReady : (params:any) => {
-				console.log('The grid is now ready'),
-				this.api = params.api;
-				this.columnApi = params.columnApi;
-				this.api.rowModel.setRowData(this.data);
-				this.api.redrawRows();
-				this.columnApi.autoSizeAllColumns();
-				this.adjustContainerSize();
-			},
-			onRowClicked: event => {
-				console.log(event);
-				this.selectedRowIndex=(event.rowIndex);
-			},
-			onColumnResized: event => {
-				console.log('A column was resized');
-				this.adjustContainerSize();
-			},
-			getRowHeight: (params) => 25
-		};
 		window.addEventListener('resize',()=>this.adjustContainerSize(), true);
+		this.gridOptions.onGridReady=(params:any) => {
+			console.log('The grid is now ready'),
+			this.api = params.api;
+			this.columnApi = params.columnApi;
+			//this.api.rowModel.setRowData(this.data);
+			//this.api.redrawRows();
+			this.columnApi.autoSizeAllColumns();
+			this.adjustContainerSize();
+		};
 		//console.log("container.offsetTop",document.getElementById("gridTable").offsetTop);
-		
 	};
 	
 	constructor(){
@@ -45,45 +28,20 @@ export class DataTableComponent implements OnInit {
 
 	}
 	
-	@Input() data:any;
+	@Input() rowData:Array<any>=[];
+	@Input() gridOptions:GridOptions<any>={};
 	@Input() deleteHandler:any;
+	@Input() selectedRowId:number | null=null;
 	
 	private api:any;
 	private columnApi:any;
 	private collumnDefs:ColDef[]=[];
-	private defaultColDef: ColDef = {
-		resizable:true,
-		sortable: true,
-		filter: false,
-		editable:false,
-	};
 
 	
 	deleteData : any=null;
-	public selectedRowIndex:number | null=null;	
-	public gridOptions!:GridOptions;
+	//public gridOptions!:GridOptions;
 	public containerStyle:any={};
 	
-	aGridTest=()=>{
-		console.log(this.api.deselectAll());
-	}
-	
-	private tableData:any={
-		getColumnDefs:()=>{
-			let tableColumn=Object.keys(this.data[0]);
-			let temp:ColDef[];
-			temp=[];
-			tableColumn.map((item:string)=>{
-				temp.push({field:item})
-			});
-			return temp;
-		}
-	};
-	
-	delete=(rowIndex:number | null)=>{
-		if(rowIndex===null)return alert(JSON.stringify("null"));
-		return this.deleteHandler(rowIndex);
-	}
 	
 	private adjustContainerSize=()=>{
 		let container=document.getElementById("gridTable")!;
@@ -106,13 +64,11 @@ export class DataTableComponent implements OnInit {
 		else{
 			width=(innerTableRectRight+scrollBarRectWidth)+"px";
 		}
-		let heightOffset:number=-5;
-		height=(window.innerHeight-containerRectTop+window.scrollY)+"px";
+		let marginBottom:number=-5;
+		height=(window.innerHeight-containerRectTop+window.scrollY)+marginBottom+"px";
 		this.containerStyle={
 			width:width,
 			height:height,
 		};
 	};
-	
-	
 }
