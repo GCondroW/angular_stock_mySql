cefcacf9e344763ed532f2c809ff31ae7d3bd709
GCondroW <116429072+GCondroW@users.noreply.github.com>
2024-01-22 11:03:02 +0700

    Delete angular directory

--- angular/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
- serverConfigDir:"../serverConfig.json",
-   PORT:2125,
-};
--- angular/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  serverConfigDir:"../../../serverConfig.json",
-  PORT:2125,
-};
